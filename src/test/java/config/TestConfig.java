6b22a0395de90e38e6b9a8b1e2481776e2bbce88
Yaroslav Kush <yaroslav_kush@epam.com>
2019-12-05 18:31:10 +0200

    [REFACTOR] Add some changes

--- src/test/java/config/TestConfig.java
+++ src/test/java/config/TestConfig.java
@@ -1,15 +1,13 @@
 package config;
 
-import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.specification.RequestSpecification;
-import utilities.RestAssureExtension;
 
 public class TestConfig {
     public static RequestSpecification videoGame_requestSpec;
 
-//    @Before
+    @Before
     public static void beforeScenario() {
         System.out.println("before");
 
@@ -21,15 +19,5 @@ public class TestConfig {
                 addHeader("Accept", "application/json").
                 build();
 
-    }
-
-    @Before
-    public static void testSetup(){
-        RestAssureExtension restAssureExtension = new RestAssureExtension();
-    }
-
-    @After
-    public static void afterScenario() {
-        System.out.println("after");
     }
 }
--- src/test/java/stepDefinition/MyStepDefs.java
+++ src/test/java/stepDefinition/MyStepDefs.java
@@ -5,43 +5,21 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.response.Response;
-import io.restassured.response.ResponseOptions;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
-import utilities.RestAssureExtension;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
+import static config.TestConfig.videoGame_requestSpec;
+import static io.restassured.RestAssured.given;
 
 public class MyStepDefs {
 
-    private static ResponseOptions<Response> response;
+    private Response response;
     private ValidatableResponse json;
     private RequestSpecification request;
 
-//    @Given("User set specification")
-//    public void user_set_specification() {
-//        request = given().spec(videoGame_requestSpec);
-//    }
-//
-//    @When("Endpoint {string} was set endpoint")
-//    public void endpoint_was_set_endpoint(String endPoint) {
-//        if (!EndPoint.contains(endPoint)) {
-//            System.out.println("Nooooo");
-//        } else
-//            response = request.get(EndPoint.END_POINT_VIDEOGAMES.getEndPoint());
-//        // Write code here that turns the phrase above into concrete actions
-//    }
-//
-//    @Then("The status code is {int}")
-//    public void theStatusCodeIsExpectedStatus(Integer inter) {
-//        System.out.println(inter);
-//        json = response.then().statusCode(inter);
-//    }
-
     @Given("User set specification")
     public void user_set_specification() {
-//        request = given().spec(videoGame_requestSpec);
+        request = given().spec(videoGame_requestSpec);
     }
 
     @When("Endpoint {string} was set endpoint")
@@ -49,35 +27,13 @@ public class MyStepDefs {
         if (!EndPoint.contains(endPoint)) {
             System.out.println("Nooooo");
         } else
-            response = RestAssureExtension.GetOps(EndPoint.END_POINT_VIDEOGAMES.getEndPoint());
+            response = request.get(EndPoint.END_POINT_VIDEOGAMES.getEndPoint());
+        // Write code here that turns the phrase above into concrete actions
     }
 
-//    @Then("The status code is '(\\d+))'")
-//    public void theStatusCodeIsExpectedStatus(Integer inter) {
-//        System.out.println(inter);
-//
-//        assertThat(response.thenReturn().statusCode(), equalTo(inter));
-//    }
-    
-
-
-    @Then("^The status code is \"([^\"]*)\"$")
-    public void theStatusCodeIsExpectedStatus(int code) {
-        System.out.println(code);
-        assertThat(response.thenReturn().statusCode(), equalTo(code));
+    @Then("The status code is {int}")
+    public void theStatusCodeIsExpectedStatus(Integer inter) {
+        System.out.println(inter);
+        json = response.then().statusCode(inter);
     }
-
-
-//    @Then("The status code is {string}")
-//    public void theStatusCodeIsExpectedStatus(String code) {
-//        System.out.println(code);
-//        assertThat(response.thenReturn().statusCode(), equalTo(int(code)));
-//    }
-
-//    @Then("The status code is {int}")
-//    public void theStatusCodeIsExpectedStatus(Integer code) {
-//        System.out.println(code);
-//        assertThat(response.thenReturn().statusCode(), equalTo(code));
-//    }
-
 }
